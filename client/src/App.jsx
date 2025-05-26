1366774b8bc1f26ef0ca43920e6990ba4c9c3e38
jai-atithya <jaiatithya@gmail.com>
2025-05-26 16:44:56 +0530

    compoenet enrolledCourse added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Login } from './components/Login'
 import { Signup } from './components/Signup'
 import { Navbar } from './components/Navbar'
+import { EnrolledCourses } from './components/EnrolledCourses'
 
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/login" element={<Login/>}></Route>
           <Route path="/signup" element={<Signup/>}></Route>
+          <Route path="/" element={<EnrolledCourses/>}></Route>
         </Routes>
       </BrowserRouter>
     </>
