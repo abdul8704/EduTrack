d722d76500e0661a261cade7f80d1699e4346b03
jai-atithya <jaiatithya@gmail.com>
2025-05-26 14:59:44 +0530

    Added browser routing app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from 'react'
 import { BrowserRouter, Routes, Route, } from 'react-router-dom'
-
+import { Navbar } from './components/Navbar.jsx'
+import { Courses } from './components/Courses.jsx'
+import { Login } from './components/Login.jsx'
+import { Singup } from './components/Signup.jsx'
 
 function AppContent() {
   return (
     <>
+      <Navbar/>
       <Routes>
-
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Singup />} />
+        <Route path='/course' element={<Courses />} />
       </Routes>
     </>
   )
