f7fc724a49cd32003664d7e27e927e33ea0a615d
Joseph Daniel <josephdaniel8805@gmail.com>
2025-05-27 16:37:26 +0530

    Home to Login connection established

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Login } from './components/Login';
-import { Signup } from './components/Signup';
 import { Navbar } from './components/Navbar';
 import { Home } from './components/Home';
 import { Footer } from './components/Footer';
@@ -12,7 +11,6 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
         <Route path="/course" element={<CoursePage />} />
         <Route path="/" element={<Home />} />
       </Routes>
