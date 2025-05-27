b42ea66b60b6ada770098c038e7dad8e7cc119e1
jai-atithya <jaiatithya@gmail.com>
2025-05-27 10:37:46 +0530

    added basic structure for enrolledCourses

--- client/src/components/EnrolledCourses.jsx
+++ client/src/components/EnrolledCourses.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 export const EnrolledCourses = () => {
   return (
-    <div>EnrolledCourses</div>
+    <div className="enrolled-container">
+      <div className="enrolled-title">Enrolled Courses</div>
+      <div className="enrolled-courses-container">
+        
+      </div>
+    </div>
   )
 }
