e9e2bf4c087b336222150d4e8e342ba37b9319ad
Keerthika <nkeerthika09@gmail.com>
2025-05-27 23:18:30 +0530

    Created profile card and employee details page

---
+++ client/src/components/ProfileCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../styles/ProfileCard.css';
+
+export const ProfileCard = ({ photo, name, designation, email }) => {
+  return (
+    <div className="profile-card">
+      <div className="profile-top"></div>
+      <img src={photo} alt={name} className="profile-photo" />
+      <div className="profile-details">
+        <h2 className="profile-name">{name}</h2>
+        <p className="profile-designation">{designation}</p>
+        <p className="profile-email">{email}</p>
+        <button className="emp-download-button">View Profile</button>
+      </div>
+    </div>
+  );
+};
