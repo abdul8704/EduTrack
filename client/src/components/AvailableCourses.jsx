09a5ebb1123808617c27c140ce76b765602c465d
jai-atithya <jaiatithya@gmail.com>
2025-05-27 12:42:07 +0530

    ...

---
+++ client/src/components/AvailableCourses.jsx
@@ -0,0 +1,97 @@
+import React, { useRef } from 'react';
+import '../styles/EnrolledCourses.css';
+
+export const AvailableCourses = () => {
+  const courses = [
+    {
+      id: 1,
+      title: 'Machine Learning Basics',
+      instructor: 'Prof. Ravindran',
+      image: 'https://www.citystgeorges.ac.uk/__data/assets/image/0010/685342/varieties/breakpoint-max.jpg',
+      rating: 4.5,
+    },
+    {
+      id: 2,
+      title: 'Deep Learning',
+      instructor: 'Andrew Ng',
+      image: 'https://wealthcreator.co.in/wp-content/uploads/2022/12/Free-Online-Courses-with-Certificates.jpg',
+      rating: 4.8,
+    },
+    {
+      id: 3,
+      title: 'Full Stack Web Dev',
+      instructor: 'Angela Yu',
+      image: 'https://www.uplers.com/wp-content/uploads/2021/12/full-stack-web-developers.png',
+      rating: 4.7,
+    },
+    {
+      id: 4,
+      title: 'Data Structures',
+      instructor: 'Robert Sedgewick',
+      image: 'https://miro.medium.com/v2/resize:fit:1400/1*J38nYZU7gzu-4lQmtjlSUw.jpeg',
+      rating: 4.3,
+    },
+    {
+      id: 5,
+      title: 'React Fundamentals',
+      instructor: 'Dan Abramov',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
+      rating: 4.6,
+    },
+    {
+      id: 6,
+      title: 'Machine Learning Basics',
+      instructor: 'Prof. Ravindran',
+      image: 'https://www.citystgeorges.ac.uk/__data/assets/image/0010/685342/varieties/breakpoint-max.jpg',
+      rating: 4.5,
+    },
+    {
+      id: 7,
+      title: 'Deep Learning',
+      instructor: 'Andrew Ng',
+      image: 'https://wealthcreator.co.in/wp-content/uploads/2022/12/Free-Online-Courses-with-Certificates.jpg',
+      rating: 4.8,
+    },
+    {
+      id: 8,
+      title: 'Full Stack Web Dev',
+      instructor: 'Angela Yu',
+      image: 'https://www.uplers.com/wp-content/uploads/2021/12/full-stack-web-developers.png',
+      rating: 4.7,
+    },
+    {
+      id: 9,
+      title: 'Data Structures',
+      instructor: 'Robert Sedgewick',
+      image: 'https://miro.medium.com/v2/resize:fit:1400/1*J38nYZU7gzu-4lQmtjlSUw.jpeg',
+      rating: 4.3,
+    },
+    {
+      id: 10,
+      title: 'React Fundamentals',
+      instructor: 'Dan Abramov',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg',
+      rating: 4.6,
+    },
+  ];
+
+  const scrollRef = useRef(null);
+
+  return (
+    <div className="enrolled-container">
+      <div className="enrolled-title">Available Courses</div>
+      <div className="available-courses-container" ref={scrollRef}>
+        {courses.map((course) => (
+          <div key={course.id} className="enrolled-course-card">
+            <img src={course.image} alt={course.title} className="enrolled-course-image" />
+            <div className="enrolled-course-title">{course.title}</div>
+            <div className="enrolled-course-bottom">
+              <div className="enrolled-course-instructor">by {course.instructor}</div>
+              <div className="enrolled-course-rating">⭐ {course.rating}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
