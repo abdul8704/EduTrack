4475239dc63d3bda12497f38f32b9c4e73706c3c
jai-atithya <jaiatithya@gmail.com>
2025-05-28 06:51:42 +0530

    ,,,...

--- client/src/components/Course.jsx
+++ client/src/components/Course.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import '../styles/course.css';
+import { CourseNavbar } from './CourseNavbar';
 
 const courseModules = [
   {
@@ -38,34 +39,17 @@ export const Course = () => {
 
   return (
     <div className="course-container">
-      <div className={`course-navbar ${isCollapsed ? 'collapsed' : ''}`}>
-        {courseModules.map((module, index) => (
-          <div key={index} className="course-module-wrapper">
-            <button
-              className="course-nav-btn"
-              onClick={() => toggleModule(index)}
-            >
-              {module.name}
-            </button>
-
-            {activeIndex === index && (
-              <div className="course-submodule-list">
-                {module.submodules.map((sub, subIndex) => (
-                  <div key={subIndex} className="course-submodule-item">
-                    {sub}
-                  </div>
-                ))}
-              </div>
-            )}
-          </div>
-        ))}
-      </div>
-              <button
-        className="course-hamburger"
-        onClick={toggleNavbar}
-      >
+      <CourseNavbar
+        modules={courseModules}
+        activeIndex={activeIndex}
+        toggleModule={toggleModule}
+        isCollapsed={isCollapsed}
+      />
+      <button className="course-hamburger" onClick={toggleNavbar}>
         {isCollapsed ? '>' : '<'}
       </button>
+
+
       <div className="course-module">
         {/* Right-side content will go here later */}
       </div>
---
+++ client/src/components/CourseNavbar.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../styles/course.css';
+
+export const CourseNavbar = ({ modules, activeIndex, toggleModule, isCollapsed }) => {
+  return (
+    <div className={`course-navbar ${isCollapsed ? 'collapsed' : ''}`}>
+      {modules.map((module, index) => (
+        <div key={index} className="course-module-wrapper">
+          <button
+            className="course-nav-btn"
+            onClick={() => toggleModule(index)}
+          >
+            {module.name}
+          </button>
+
+          {activeIndex === index && (
+            <div className="course-submodule-list">
+              {module.submodules.map((sub, subIndex) => (
+                <div key={subIndex} className="course-submodule-item">
+                  {sub}
+                </div>
+              ))}
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
