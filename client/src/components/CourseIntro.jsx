c3e131c04bca259ce881ca27b63e17391ca21110
Harini <harinisankar1712@gmail.com>
2025-05-28 11:35:48 +0530

    created courseoverview

--- client/src/components/CourseIntro.jsx
+++ client/src/components/CourseIntro.jsx
@@ -1,18 +1,16 @@
 import React from 'react'
 import '../styles/CoursePage.css';
 import IotImage from '../assets/zuntraLogo.avif';
-export const CourseIntro = ({name}) => {
+export const CourseIntro = ({ name, img, info,label }) => {
     return (
         <div className="course-page">
             <div className="course-card">
-                <div className="course-info corner-layout">
-                    <img className="big-image" src={IotImage} alt="IoT" />
+                <div className="course-info corner-layout"  >
+                    <img className="big-image" src={img} alt="IoT" />
                     <div className="text-corner">
                         <h2>{name}</h2>
                         <p>
-                            This course enriches the learners to describe components of IoT Architecture and platforms of IoT ecosystem, and understand how to use Arduino to build prototypes.
-
-                            This comprehensive course is designed to provide learners with a deep understanding of the fundamental components and architecture of the Internet of Things (IoT). You will explore various platforms and ecosystems that support IoT development, including cloud services, communication protocols, and data analytics tools.
+                            {info}
                         </p>
                         <button className="start-button">Start</button>
                     </div>
