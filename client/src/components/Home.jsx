59bdc62be7ba254bbddc18f5b4446b31ad9a2350
jai-atithya <jaiatithya@gmail.com>
2025-05-28 11:20:16 +0530

    ...

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import { EnrolledCourses } from './EnrolledCourses'
 import { AvailableCourses } from './AvailableCourses'
+import axios from 'axios'
+
+const getCourseData = async (req, res) => {
+  const data = await axios.get('/api/user/')
+  console.log(data);
+  return data;
+}
 
 export const Home = () => {
+  const course = getCourseData();
   return (
     <>    
         <EnrolledCourses/>
