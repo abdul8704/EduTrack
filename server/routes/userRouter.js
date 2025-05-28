957e23bf80a702ddf942488aa4553d08487a9bdf
jai-atithya <jaiatithya@gmail.com>
2025-05-28 17:23:03 +0530

    kdjashfkjaf

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -5,8 +5,8 @@ const userController = require("../controllers/user");
 router.get("/:userid", userController.getAllCourses);
 router.get("/:userid/:courseId", userController.getCourseById);
 router.get(
-    "/:userid/:courseId/module/:moduleNumber",
-    userController.getModuleByCourseId
+    "/:userid/:courseId/module/:moduleNumber/:subModuleNumber",
+    userController.getSubModuleByCourseId
 );
 
 module.exports = router;
