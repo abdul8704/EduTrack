2850520931b0ae32c432d6b368cd3bbc5229a4ca
jai-atithya <jaiatithya@gmail.com>
2025-05-30 05:33:16 +0530

    everything almost done

--- client/src/components/CourseDeets.jsx
+++ client/src/components/CourseDeets.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import '../styles/coursedeets.css';
-import defaultImage from '../assets/react.svg'; // Adjust the path as necessary
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import axios from "axios"
