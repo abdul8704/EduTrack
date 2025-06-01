cd91f95b49db7bb9049beb226953b261e5150883
jai-atithya <jaiatithya@gmail.com>
2025-06-01 20:56:24 +0530

    FORM RESOLVED

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import { CourseIntro } from './pages/CourseIntro';
 import { CourseLearn } from './pages/CourseLearn';
 import { EmpProgress } from './pages/EmpProgress';
 import { AdminDashboard } from './pages/AdminDashboard';
-import  AddCourse  from './pages/AddCourse'; 
+import  { AddCourse }  from './pages/AddCourse'; 
 import { CourseDeets } from './pages/CourseDeets';
 import { AvailableCourses } from "./components/AvailableCourses";
 import { Profile } from "./pages/Profile";
