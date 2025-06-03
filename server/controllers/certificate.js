4ff27ccaaab62099a85a38c1b24daa8bd0c5bc35
abdul8704 <abdulaziz87049@gmail.com>
2025-06-03 14:18:50 +0530

    puppeteer Bug fix

--- server/controllers/certificate.js
+++ server/controllers/certificate.js
@@ -2,7 +2,7 @@ const puppeteer = require("puppeteer");
 
 const generateCertificate = async (req, res) => {
   console.log("Puppeteer executable path:", puppeteer.executablePath());
-
+  console.log("Received certificate request:", req.body);
     try {
         const { name, course, date, instructor } = req.body;
 
