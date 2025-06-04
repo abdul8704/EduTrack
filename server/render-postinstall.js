7b7e825ecab5717260bcb20c201089eb6b177177
abdul8704 <abdulaziz87049@gmail.com>
2025-06-04 20:10:13 +0530

    making the server cross-platform compatible

---
+++ server/render-postinstall.js
@@ -0,0 +1,11 @@
+const os = require("os");  // install puppeteer chromium on linux explicity, dont need this for windows...
+const { execSync } = require("child_process");
+
+const platform = os.platform(); // 'linux', 'win32', 'darwin', etc.
+
+if (platform === "linux") {
+    console.log("Installing Chromium for Puppeteer (Linux)...");
+    execSync("npx puppeteer browsers install chrome", { stdio: "inherit" });
+} else {
+    console.log(`Skipping Puppeteer install for platform: ${platform}`);
+}
